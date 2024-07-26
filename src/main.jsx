0e10c7b1f0f36249782f27dfb66e4a3364dc14b0
myunghun choi <myunghun0721@gmail.com>
2024-07-26 12:19:35 -0700

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDom from 'react-dom/client'
+import { App } from './App';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDom.createRoot(document.getElementById('root')).render(<App />);
