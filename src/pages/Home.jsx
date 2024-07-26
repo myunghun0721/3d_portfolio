ae1231a138342905f20dda52c224eb0708bdef73
myunghun choi <myunghun0721@gmail.com>
2024-07-26 13:16:11 -0700

    import model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import { Canvas } from '@react-three/fiber'
 import React, { Suspense } from 'react'
 import Loader from '../components/Loader'
+import Island from '../models/Island'
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null
+    let screenPosition = [0, -6.5, -43]
+    let rotation = [0.1, 4.7, 0]
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9]
+    } else {
+      screenScale = [1, 1, 1]
+    }
+
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className='w-full h-screen relative'>
       {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -18,6 +35,8 @@ const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight />
+
+          <Island position={islandPosition} scale={islandScale} rotation={islandRotation} />
         </Suspense>
       </Canvas>
     </section>
