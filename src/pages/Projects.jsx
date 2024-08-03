b2283b8fe83ce907c96efc5f2fe22a21291de2ce
myunghun choi <myunghun0721@gmail.com>
2024-08-02 18:33:23 -0700

    project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        My <span className='blue-gradient_text font-semibold drop-shadow'>Projects</span>
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          " I am pleased to introduce my latest achievements: three meticulously crafted full-stack web applications, each developed to integrate front-end and back-end technologies seamlessly. These projects showcase my ability to deliver high-quality, functional, and user-friendly web applications."
+        </p>
+      </div>
+    </section>
   )
 }
 
